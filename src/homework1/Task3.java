8586aa9ada286002acbd957247379b9adaa93330
Kseniya <ksberezovik@gmail.com>
2017-02-17 12:38:37 +0300

    add Task3

--- src/homework1/Task3.java
+++ src/homework1/Task3.java
@@ -3,24 +3,38 @@ package homework1;
 import java.util.Scanner;
 
 public class Task3 {
-    public static boolean isPositive(int number) {
+    public static boolean isPositive(double number) {
         return number > 0;
     }
 
-    public static boolean isZero(int number) {
+    public static boolean isZero(double number) {
         return number == 0;
     }
 
-    public static int discriminant(int a, int b, int c) {
-        return 0;
+    public static double discriminant(double a, double b, double c) {
+        return b * b - 4 * a * c;
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int a = scanner.nextInt();
-        int b = scanner.nextInt();
-        int c = scanner.nextInt();
+        double a = scanner.nextDouble();
+        double b = scanner.nextDouble();
+        double c = scanner.nextDouble();
+        scanner.close();
 
+        double d = discriminant(a, b, c);
 
+        if (isZero(d)) {
+            double x = -b / (2 * a);
+            System.out.println(String.format("Уравнение имеет один корень: %f", x));
+        } else {
+            if (!isPositive(d)) {
+                System.out.println("Уравнение не имеет действительных корней");
+            } else {
+                double x1 = (-b + Math.sqrt(d)) / (2 * a);
+                double x2 = (-b - Math.sqrt(d)) / (2 * a);
+                System.out.println(String.format("Уравнение имеет два кореня: %f; %f", x1, x2));
+            }
+        }
     }
 }
