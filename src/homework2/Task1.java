f5dff70e889f5ed046c0bc3e360f57daa7021f0d
Kseniya <ksberezovik@gmail.com>
2017-02-18 10:21:05 +0300

    add Task1

---
+++ src/homework2/Task1.java
@@ -0,0 +1,20 @@
+package homework2;
+
+import java.util.Scanner;
+
+public class Task1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        scanner.close();
+
+        int sum = 0;
+
+        while (number > 0) {
+            sum += number % 10;
+            number /= 10;
+        }
+
+        System.out.println(sum);
+    }
+}
